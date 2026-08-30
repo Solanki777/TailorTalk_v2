052f7a09b535566412a1ee3c67f8669b639b82c0
Solanki777 <solankimaheshkhash7@gmail.com>
2026-08-30 21:51:33 +0530

    learn load_dotenv for environemtn varibale setup

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+APP_NAME = os.getenv("APP_NAME","TailorTalk")
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from backend.services import greeting
 from backend.api.routes.chat import router as chat_router
+from backend.config import *
 
-app = FastAPI()
+app = FastAPI(title=APP_NAME)
 
 @app.get("/")
 def home():
-    return {"message":greeting.get_gretting()}
+    return {"message":f"Welcome to {APP_NAME} 2.0"}
 
 # @app.post("/api/v1/chat")
 # def chat(request:ChatRequest):
