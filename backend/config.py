edd60a21801be401a08c5995c8562b59ad3e5a28
Solanki777 <solankimaheshkhash7@gmail.com>
2026-09-04 21:11:53 +0530

    groq api testing

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,9 @@
-import os 
+import os
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
-APP_NAME = os.getenv("APP_NAME","TailorTalk")
+APP_NAME = os.getenv("APP_NAME", "TailorTalk")
 ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
--- backend/services/llm/groq.py
+++ backend/services/llm/groq.py
@@ -1,7 +1,24 @@
+from groq import Groq
+
+from backend.config import GROQ_API_KEY
 from backend.services.llm.base import LLMProvider
 
 
 class GroqProvider(LLMProvider):
 
+    def __init__(self):
+        self.client = Groq(api_key=GROQ_API_KEY)
+
     def generate(self, prompt: str):
-        return f"Groq received: {prompt}"
+
+        response = self.client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        )
+
+        return response.choices[0].message.content
